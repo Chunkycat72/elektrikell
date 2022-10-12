8b612cc674b4a119a5d0271d019c9417e92fce12
Alex Kvasha <alex.kofrik@gmail.com>
2022-10-12 18:22:39 +0300

    homework

---
+++ src/Body/index.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import Row from 'react-bootstrap/Row';
+
+function Body() {
+
+  return (
+    <>
+        <Row>
+            <h2>Future graph area</h2>
+        </Row>
+      </>)}
+
+export default Body;
