63e540460e6d42f41cba8df2e78614e96b95f985
Alex Kvasha <alex.kofrik@gmail.com>
2022-10-17 20:42:54 +0300

    api integration

---
+++ src/services/apiservice.js
@@ -0,0 +1,18 @@
+import moment from "moment";
+
+const apiurl = 'https://dashboard.elering.ee/api';
+
+export async function getCurrentPrice() {
+    const country = 'EE';
+    const response = await fetch(`${apiurl}/nps/price/${country}/current`);
+    return response.json();
+};
+
+
+export async function getPriceData() {
+    const start = moment().utc().subtract(10, 'hours').format();
+    const end = moment().utc().add(30, 'hours').format();
+    const params = new URLSearchParams({start, end});
+    const response = await fetch(`${apiurl}/nps/price?${params}`);
+    return response.json();
+}
